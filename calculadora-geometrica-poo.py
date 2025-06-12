3b110bcb937791c40e042f82406b574c2d3e56d7
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-06-12 13:54:03 -0300

    Update calculadora-geometrica-poo.py

--- calculadora-geometrica-poo.py
+++ calculadora-geometrica-poo.py
@@ -2,11 +2,13 @@ import math
 import numpy as np
 
 class Reta:
-    def __init__(self,x1,y1,x2,y2):
+    def __init__(self,x1,y1,x2,y2,x3,y3):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
     
     def mostrar_valores(self):
         print(f"Sua reta será composta pelos pontos:A= ({self.x1} , {self.y1}) e B= ({self.x2} , {self.y2})")
@@ -21,27 +23,70 @@ class Reta:
         print(f"O ponto médio do eixo 'x' da reta é {ponto_medio_x} e do eixo 'y' é {ponto_medio_y}")
 
     def pontos_colineares(self):
-        
+        matriz = np.array([[self.x1,self.y1,1],[self.x2,self.y2,1],[self.x3,self.y3,1]])
+        determinante = np.linalg.det(matriz)
+        if determinante == 0:
+            print("Os pontos são colineares, ou seja, estão alinhados na mesma reta")
+        else:
+            print("Os pontos não são colineares")
+    
+    def inclinacao_reta(self):
+        if self.x1 == self.x2:
+            print("Inclinação indefinida (divisão por zero = reta vertical)")
+        else:
+            inclinacao = ((self.y2)-(self.y1))/((self.x2)-(self.x1))
+            print(f"A inclinação da reta (coeficiente angular) é dada por (y2 - y1) / (x2 - x1) = {inclinacao}")
+            print(f"A inclinação da reta é de ({self.y2}-{self.y1}/{self.x2}-{self.x1}) = {inclinacao}")
+
+def entrada_2_valores():
+    try:
+        x1 = float(input("Digite o x do ponto A"))
+        y1 = float(input("Digite o y do ponto A"))
+        x2 = float(input("Digite o x do ponto B"))
+        y2 = float(input("Digite o y do ponto B"))
+    
+        criacao_reta = Reta(x1,y1,x2,y2)
+        criacao_reta.mostrar_valores()
+        return criacao_reta
+    except ValueError:
+        print("Caractere inválido! Digite um número")
+
+def entrada_3_valores():
+    try:
+        x1 = float(input("Digite o x do ponto A"))
+        y1 = float(input("Digite o y do ponto A"))
+        x2 = float(input("Digite o x do ponto B"))
+        y2 = float(input("Digite o y do ponto B"))
+        x3 = float(input("Digite o x do ponto C"))
+        y3 = float(input("Digite o y do ponto C"))
+
+        criacao_reta = Reta(x1,y1,x2,y2,x3,y3)
+        criacao_reta.mostrar_valores()
+        return criacao_reta
+    except ValueError:
+        print("Caractere inválido! Digite um número")
+
 def main():
     while True:
-        entrada = input("CALCULADORA GEOMÉTRICA\nDigite ___ para:\n1. Calcular a DISTÂNCIA ENTRE DOIS PONTOS NA RETA\n2. Calcular o PONTO MÉDIO ENTRE DOIS PONTOS NA RETA\n3. Calcular os PONTOS COLINEARES\n4. Calcular a INCLINAÇÃO DA RETA\n0. Para ENCERRAR\n")
-        if entrada =="1":
-            reta_criada = entrada_valores()
-            reta_criada.distancia_dois_pontos()
-        elif entrada == "2":
-            reta_criada = entrada_valores()
-            reta_criada.ponto_medio()
-
-def entrada_valores():
-    x1 = float(input("Digite o x do ponto A"))
-    y1 = float(input("Digite o y do ponto A"))
-    x2 = float(input("Digite o x do ponto B"))
-    y2 = float(input("Digite o y do ponto B"))
-    
-    criacao_reta = Reta(x1,y1,x2,y2)
-    criacao_reta.mostrar_valores()
-    return criacao_reta
-
+        try:
+            entrada = int(input("CALCULADORA GEOMÉTRICA\nDigite ___ para:\n1. Calcular a DISTÂNCIA ENTRE DOIS PONTOS NA RETA\n2. Calcular o PONTO MÉDIO ENTRE DOIS PONTOS NA RETA\n3. Calcular os PONTOS COLINEARES\n4. Calcular a INCLINAÇÃO DA RETA\n0. Para ENCERRAR\n"))
+            if entrada =="1":
+                reta_criada = entrada_2_valores()
+                reta_criada.distancia_dois_pontos()
+            elif entrada == "2":
+                reta_criada = entrada_2_valores()
+                reta_criada.ponto_medio()
+            elif entrada == "3":
+                reta_criada = entrada_3_valores()
+                reta_criada.pontos_colineares()
+            elif entrada == "4":
+                reta_criada = entrada_2_valores
+                reta_criada.inclinacao_reta()
+            else:
+                print("Digite um número válido!")
+                continue
+        except ValueError:
+            print("Caractere inválido! Digite o que deseja fazer de acordo com o MENU\n")
 main()
 
         
